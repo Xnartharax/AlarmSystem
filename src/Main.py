f49bc62eb7851a43ac0472b72dec9f17c2d8445b
jonas <jonas@treplin.de>
2020-03-17 18:30:31 +0100

    config is now permanent

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,5 @@
 from kivy.config import Config
-Config.set('kivy', 'keyboard_mode', 'systemanddocked')
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+Config.write()
 from hausnotruf import engine, TestApp
 TestApp().run()
