acde1ede35d86f85f2fb7dd9911937c2e5f96af2
jonas <jonas@treplin.de>
2020-03-23 11:53:51 +0100

    added restart via pid

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,9 @@
 from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 Config.write()
+import os
+pid_fp = open("./hausnortuf_pid.txt", "w+")
+pid_fp.write(os.getpid()) # to kill and restart the process after update
+pid_fp.close()
 from hausnotruf import engine, TestApp
 TestApp().run()
--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ cd ${DIR}
 echo ${DIR}
 git fetch
 git checkout HEAD ${DIR}/src/
+cd src
+kill -9 < hausnortuf_pid.txt
+sudo python3.6 Main.py
