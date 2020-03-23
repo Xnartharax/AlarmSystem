ef97ffb5a1caa20252b286a947b759d9fb4dcf7b
jonas <jonas@treplin.de>
2020-03-23 11:55:39 +0100

    pid to string

--- src/Main.py
+++ src/Main.py
@@ -3,7 +3,7 @@ Config.set('kivy', 'keyboard_mode', 'systemanddock')
 Config.write()
 import os
 pid_fp = open("./hausnortuf_pid.txt", "w+")
-pid_fp.write(os.getpid()) # to kill and restart the process after update
+pid_fp.write(str(os.getpid())) # to kill and restart the process after update
 pid_fp.close()
 from hausnotruf import engine, TestApp
 TestApp().run()
